14503e4e1e117fe81eff3dec5fded5972ecf2003
Lucas Jung <hongcoffee@gmail.com>
2012-11-17 23:52:30 +0900

    불필요 파일 삭제

--- opentrade/modules/gnb-logout.php
+++
@@ -1,9 +0,0 @@
-<!-- gnb s -->
-<div class="gnb navbar navbar-inverse navbar-fixed-top">
-	<div class="navbar-inner">
-		<div class="container">
-			<a class="brand" href="/">Open<b>Trade</b></a>
-		</div>
-	</div>
-</div>
-<!-- gnb e -->
