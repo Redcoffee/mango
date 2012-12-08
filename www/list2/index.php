5bec93d8afd49650a84f47d4bcab17346b10c9b9
Lucas Jung <hongcoffee@gmail.com>
2012-12-08 21:31:53 +0900

    list, list2와 디렉토리명 교환

--- www/list2/index.php
+++ www/list2/index.php
@@ -2,137 +2,258 @@
 $g->page->js[] = 'http://static.dofork.com/mobile/js/fastclick.js';
 $g->page->css[] = './list.css';
 $g->page->display();
-
-$query = "select * from redcoffee.item order by count desc";
-$results = $g->db->fetch_all($query);
 ?>
 <header class="navbar-fixed-top">
     <a href="#" class="btn-line pull-left">뒤로</a>
-    <a href="#" class="btn-line pull-right cart">장바구니<span id="cart-badge" class="badge"></span></a>
+    <a href="#" class="btn-line pull-right">장바구니</a>
     <h1><b>망고</b>마트</h1>
 </header>
 <section class="body">
     <ul class="list">
-        <?
-        foreach ($results as $item):
-            $thumb_url = "/files/r".$item[id].".jpg";
-        ?>
-        <li id="<?=$item[id]?>" data-item-id="<?=$item[id]?>">
-            <!--span class="chk spr pull-left"></span-->
+        <li>
+            <span class="chk spr pull-left"></span>
             <a href="#" class="arrow spr pull-right"></a>
-            <span class="stock pull-right">v</span>
+            <select class="stock pull-right">
+                <option>x1</option>
+                <option>x2</option>
+                <option>x3</option>
+                <option>x4</option>
+                <option>x5</option>
+            </select>
             <div class="item-wrap">
-                <div class="thumb pull-left"><img src="<?=$thumb_url?>"></div>
+                <div class="thumb pull-left"><img src="/files/r1.jpg"></div>
                 <div class="inner-wrap">
-                    <h3 class="item">
-                        <?=$item[name]?>
-                        <?=$item[package] ? "(".$item[package].")" : ""?>
-                    </h3>
-                    <p class="price">
-                        <span class="sale"><em><?=number_format($item[price_sale])?></em>원</span>
-                        <? if ($item[price_origin]): ?>
-                        <span class="origin"><em><?=number_format($item[price_origin])?></em>원</span>
-                        <? endif; ?>
-                    </p>
-                    <p class="count"><?=number_format($item[count])?>개 판매</p>
+                    <h3 class="item">농심 신라면(x5)</h3>
+                    <p class="price">3,170원</p>
                 </div>
             </div>
         </li>
-        <? endforeach; ?>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r4.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">농심 얼큰한 너구리(x5)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r2.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">농심 너구리 순한맛(x5)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r3.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">삼양 라면(x5)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r5.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">농심 오징어 짬뽕(x5)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r6.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">팔도 남자라면 화끈한 맛 화끈한 맛(x5)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r7.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">팔도 꼬꼬면(x5)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r8.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">농심 짜파게티(x5)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r9.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">팔도 비빔면(x5)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r10.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">농심 육개장 사발면</h3>
+                    <p class="price">670원</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <span class="chk spr pull-left"></span>
+            <a href="#" class="arrow spr pull-right"></a>
+            <select class="stock pull-right">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <div class="item-wrap">
+                <div class="thumb pull-left"><img src="/files/r11.jpg"></div>
+                <div class="inner-wrap">
+                    <h3 class="item">농심 신라면 컵(x6)</h3>
+                    <p class="price">3,170원</p>
+                </div>
+            </div>
+        </li>
     </ul>
 </section>
 
 <script type="text/javascript">
-    var user = 'redcoffee';
-
-    try {
-        var cart = JSON.parse(localStorage.getItem(user));
-        for (var i = 0; i < cart.item.length; i++) {
-            $list = $('li[data-item-id |= '+cart.item[i]+']');
-            if ($list.length) list_toggle($list);
-        };
-    }catch(e){
-        localStorage.clear();
-        //todo : 서버에 저장된 정보가 있으면 새롭게 가져오기
-        var cart = new Object();
-        cart.item = new Array();
-    }
-    cart_badge_update(cart.item.length);
-
-    function list_toggle(obj) {
-        var $self = $(obj)
-            , $prev = $self.prev()
-            , $next = $self.next();
-
-        // 클래스 처리
-        $self.toggleClass('active');
-        $self.removeClass('shade');
-
-        // 그림자 처리
-        switch ($self[0].className) {
-            case 'active':
-                if (!$prev.hasClass('active')) {
-                    $self.addClass('shade');
-                }
-                if ($next.hasClass('active')) {
-                    $next.removeClass('shade');
-                }
-            break;
-            case '':
-                if ($next.hasClass('active')) {
-                    $next.addClass('shade');
-                }
-            break;
-        }
-    }
-
-    function cart_badge_update(count) {
-        // 장바구니 배지 표시
-        var $cart_badge = $('#cart-badge');
-
-        if (count > 0) {
-            $cart_badge.html(count);
-            $cart_badge.fadeIn('fast');
-        } else if (count <= 0) {
-            $cart_badge.fadeOut('fast');
-            $cart_badge.html();
-        }
-    }
-
-    // 리스트 클릭 처리
     $('li > .item-wrap').each( function() {
         new FastClick(this);
-
         $(this).click( function() {
 
-            var $parent = $(this).parent()
-                , item_id = $parent.attr('id');
+            var $parent = $(this).parent();
+            var $prev = $parent.prev();
+            var $next = $parent.next();
 
-            // 리스트 토글
-            list_toggle($parent);
+            $parent.find('select').val(1); //수량 초기화
+            $parent.toggleClass('active');
+            $parent.removeClass('shade');
 
-
-
-            if ($parent.is('.active')) {
-                // 장바구니에 아이템 추가
-                if (cart.item.indexOf(item_id)<0) cart.item.push(item_id);
-            } else {
-                // 장바구니에 아이템 제거
-                cart.item.splice(cart.item.indexOf(item_id),1);
+            switch ($parent[0].className) {
+                case 'active':
+                    if (!$prev.hasClass('active')) {
+                        $parent.addClass('shade');
+                    }
+                    if ($next.hasClass('active')) {
+                        $next.removeClass('shade');
+                    }
+                break;
+                case '':
+                    if ($next.hasClass('active')) {
+                        $next.addClass('shade');
+                    }
+                break;
             }
-
-            cart.timestamp = new Date().getTime();
-
-            localStorage.setItem(user,JSON.stringify(cart));
-
-            console.log(cart);
-            console.log(cart.item);
-
-            // 장바구니 배지 표시
-            cart_badge_update(cart.item.length);
         });
     });
-
+/*
+    $('select.stock').each( function(){
+        var title = $(this).attr('title');
+        if( $('option:selected', this).val() != '') title = $('option:selected',this).text();
+        $(this)
+            .css({'z-index':10,'opacity':0,'-khtml-appearance':'none'})
+            .after('<span class="stock">' + title + '</span>')
+            .change( function(){
+                val = $('option:selected',this).text();
+                $(this).next().text(val);
+                })
+    });
+*/
 </script>
 
 
